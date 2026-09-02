6bb3da591f180f18190f6790adfd003e8f3b3b21
Vviikarm P Jhala <VviikarmPJhala@gmail.com>
2026-09-02 21:12:32 +0530

    subnet and vnet aded in this

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -1,5 +1,17 @@
 #This is module for dev environment put right dependency and variable values in this module. 
-module "dev-rgs" {
+module "dev_rgs" {
   source  = "../../modules/azurerm_resource_group"
   dev_rgs = var.dev_rgs
+}
+
+module "dev_vnets" {
+  depends_on     = [module.dev_rgs]
+  source         = "../../modules/azurerm_virtual_network"
+  dev_mono_vnets = var.dev_mono_vnets
+}
+
+module "dev_subnet" {
+  depends_on      = [module.dev_rgs, module.dev_vnets]
+  source          = "../../modules/azurerm_subnet"
+  dev_mono_subnet = var.dev_mono_subnet
 }
---
+++ modules/azurerm_subnet/variable.tf
@@ -0,0 +1,4 @@
+variable "dev_mono_subnet"{
+  type =map(any)
+  description = "this is frontend-subnet with address prefixes [10.0.1.0/24]"
+}
